813c9975654abee57a2f1a29eb0fe77a122f1d73
Stverib <1716203225@qq.com>
2025-04-22 17:28:30 +0800

    团队成员完成2

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -3,7 +3,7 @@
     <!-- 菜单导航 -->
     <el-menu
       :default-active="activeCategory"
-      mode="vertical"
+      mode="horizontal"
       class="category-menu"
       @select="handleSelect"
     >
@@ -47,7 +47,7 @@
 <script setup>
 import {ref} from 'vue'
 
-// 真实项目数据
+// 项目
 const projects = ref([
   // ========== 国家级项目 ==========
   {
@@ -265,6 +265,261 @@ const projects = ref([
   }
 ])
 
+const newProjectsData = ref([
+  {
+    "id": "1",
+    "department": "国家科技部",
+    "projectName": "科技创新2030-'新一代人工智能'重大项目",
+    "projectNumber": "2021ZD0111405",
+    "description": "面向节假日城市旅游客流调控和智能服务的应用示范",
+    "startTime": "2021年12月",
+    "endTime": "2024年11月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "2",
+    "department": "国家科技部",
+    "projectName": "国家重点研发计划'数据科学的若干基础理论'",
+    "projectNumber": "2018YFB1003205",
+    "description": "大数据权属保护理论与方法",
+    "startTime": "2018-05",
+    "endTime": "2021-04",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "3",
+    "department": "国家文化和旅游部",
+    "projectName": "科技创新工程项目",
+    "projectNumber": "",
+    "description": "基于多源数据的旅游决策技术研究",
+    "startTime": "2021年6月",
+    "endTime": "2023年6月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "4",
+    "department": "甘肃省委组织部",
+    "projectName": "2023年省级重点人才项目",
+    "projectNumber": "",
+    "description": "大数据背景下数据安全及个人隐私保护研究",
+    "startTime": "2023年1月",
+    "endTime": "2024年6月",
+    "role": "主持",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "5",
+    "department": "甘肃省委组织部",
+    "projectName": "甘肃省重点人才资金项目",
+    "projectNumber": "",
+    "description": "大数据背景下个人隐私保护研究团队建设项目",
+    "startTime": "2018年12月",
+    "endTime": "2020年12月",
+    "role": "主持",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "6",
+    "department": "甘肃省科技厅",
+    "projectName": "重点研发计划项目",
+    "projectNumber": "21YF5FA111",
+    "description": "基于概率近似正确理论的旅游大数据分析研究与应用",
+    "startTime": "2021年12月",
+    "endTime": "2023年12月",
+    "role": "主持",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "7",
+    "department": "甘肃省工业和信息化厅",
+    "projectName": "级制造业高质量发展和数据信息产业重点项目",
+    "projectNumber": "",
+    "description": "面向'中心-偏远地区'优质资源均衡的5G+智慧教育创新试点",
+    "startTime": "2022年1月",
+    "endTime": "2025年1月",
+    "role": "主持",
+    "category": "provincial",
+    "status": "ongoing"
+  },
+  {
+    "id": "8",
+    "department": "甘肃省教育厅",
+    "projectName": "2023年甘肃省高校产业支撑计划项目",
+    "projectNumber": "2023CYZC-01",
+    "description": "基于'5G+智慧教育'促进高质量教育均衡发展的创新试点",
+    "startTime": "2023年12月",
+    "endTime": "2026年12月",
+    "role": "参与",
+    "category": "provincial",
+    "status": "ongoing"
+  },
+  {
+    "id": "9",
+    "department": "兰州市科技局",
+    "projectName": "兰州市人才创新创业项目",
+    "projectNumber": "2021-RC-91",
+    "description": "旅游大数据关键技术研究与应用",
+    "startTime": "2021年11月",
+    "endTime": "2024年5月",
+    "role": "主持",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "10",
+    "department": "国家科学技术部",
+    "projectName": "国家科技支撑计划",
+    "projectNumber": "2013BAH40F01",
+    "description": "丝绸之路文化主题创意关键技术研究",
+    "startTime": "2013年01月",
+    "endTime": "2015年12月",
+    "role": "参与",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "11",
+    "department": "国家自然科学基金委员会",
+    "projectName": "重大研究计划面上项目课题",
+    "projectNumber": "90812001",
+    "description": "网格标准基础研究",
+    "startTime": "2011年01月",
+    "endTime": "2012年06月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "12",
+    "department": "国家自然科学基金委员会",
+    "projectName": "青年科学基金项目",
+    "projectNumber": "61300230",
+    "description": "裂痕故障块：一种面向二维或多维网格网络拓扑的容错自适应路由机制的研究与应用",
+    "startTime": "2014年01月",
+    "endTime": "2016年12月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "13",
+    "department": "国家自然科学基金委员会",
+    "projectName": "面上项目",
+    "projectNumber": "",
+    "description": "风沙环境中基于机会通信的无线传感器网络组网问题的基础研究及示范性应用",
+    "startTime": "2010年01月",
+    "endTime": "2013年12月",
+    "role": "参与",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "14",
+    "department": "国家自然科学基金委员会",
+    "projectName": "面上项目",
+    "projectNumber": "61370219",
+    "description": "面向网络规划的演化程序开发理论与方法",
+    "startTime": "2014年01月",
+    "endTime": "2017年12月",
+    "role": "参与",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "15",
+    "department": "国家人力资源社会保障部",
+    "projectName": "2017年度中国留学回国创业启动支持计划优秀项目",
+    "projectNumber": "",
+    "description": "基于语音交互的智慧家庭系统",
+    "startTime": "2017年1月",
+    "endTime": "2019年1月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "16",
+    "department": "国家工业和信息化部",
+    "projectName": "电子信息产业发展基金",
+    "projectNumber": "",
+    "description": "万维自主版式电子文件管理系统开发",
+    "startTime": "2011年10月",
+    "endTime": "2012年07月",
+    "role": "主持",
+    "category": "national",
+    "status": "completed"
+  },
+  {
+    "id": "17",
+    "department": "甘肃省科学技术厅",
+    "projectName": "重大专项",
+    "projectNumber": "1102FKDA010",
+    "description": "甘肃地质灾害防治区泥石流临灾监测预警关键技术研究与示范应用",
+    "startTime": "2012年04月",
+    "endTime": "2014年12月",
+    "role": "参与",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "18",
+    "department": "甘肃省科学技术厅",
+    "projectName": "自然科学基金",
+    "projectNumber": "",
+    "description": "基于机会通信的风沙监测无线传感器组网关键技术研究",
+    "startTime": "2011-01",
+    "endTime": "2013-12",
+    "role": "主持",
+    "category": "provincial",
+    "status": "completed"
+  },
+  {
+    "id": "19",
+    "department": "企事业委托",
+    "projectName": "清华大学",
+    "projectNumber": "",
+    "description": "数据同步系统测试",
+    "startTime": "2013年10月",
+    "endTime": "2014年06月",
+    "role": "主持",
+    "category": "enterprise",
+    "status": "completed"
+  },
+  {
+    "id": "20",
+    "department": "企事业委托",
+    "projectName": "清华大学",
+    "projectNumber": "",
+    "description": "TStor分布式文件系统的评测与建模",
+    "startTime": "2017-09",
+    "endTime": "2019-12",
+    "role": "主持",
+    "category": "enterprise",
+    "status": "completed"
+  },
+  {
+    "id": "21",
+    "department": "企事业委托",
+    "projectName": "科大讯飞",
+    "projectNumber": "",
+    "description": "语音及语言云计算关键技术研究",
+    "startTime": "2013-01",
+    "endTime": "2015-12",
+    "role": "主持",
+    "category": "enterprise",
+    "status": "completed"
+  }
+])
+
 // 项目分类
 const activeCategory = ref('national')
 
@@ -294,11 +549,9 @@ const handleSelect = (key) => {
 
 <style lang="scss" scoped>
 .projects-container {
-  width: 800px;
-  padding: 20px;
+  width: 80%;
 
   .category-menu {
-    margin-bottom: 20px;
     border-bottom: none;
 
     // 菜单项样式
